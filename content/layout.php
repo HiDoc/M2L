d0074b6e0355ef8264588d1dfce99a2de581b634
HiDoc <sound6tem@hotmail.co.uk>
2016-10-03 09:25:38 +0200

    Création du dossier de base
    Ajout de tout les dossier et de tout les fichiers

---
+++ content/layout.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Icone et titre -->
+		<link rel="icon" type="image/png" href="favicon.png" />
+		<title>M2L Gestion</title>
+		<!-- Mise en page -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-side">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					  <a class="navbar-brand" href="#">Tableau de bord</a>
+				</div>
+				<ul class="nav navbar-nav center-text">
+					<li class="active"><a href="#">Accueil</a></li>
+					<li><a href="#">Offre de formation</a></li>
+					<li><a href="#">Historique des formations</a></li> 
+					<li>
+    				</li> 
+				</ul><!-- /nav-bar-->
+			</div><!-- /container-fluid-->
+		</nav>
+		<section class="section">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-6">
+						<h1>Hello, world!</h1>
+					</div><!-- /COL -->
+					<div class="col-xs-2 col-sm-offset-4">
+						<div class="input-group">
+    						<span class="input-group-btn">
+        						<button class="btn btn-default" type="button">
+        							<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+        						</button>
+      						</span>
+      						<input type="text" class="form-control" placeholder="Rechercher...">
+    					</div><!-- /input-group -->
+					</div><!-- /COL -->
+				</div><!-- /ROW -->
+			</div><!-- /Container-Fluid -->
+		</section>
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<!-- Plugins jQuery -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
