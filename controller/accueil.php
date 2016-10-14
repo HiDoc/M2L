e5249daaf18a3fff4a5c2d9cc3d74c0221c4de55
HiDoc <sound6tem@hotmail.co.uk>
2016-10-14 09:15:28 +0200

    Rewrite url

--- controller/accueil.php
+++ controller/accueil.php
@@ -1,3 +1,74 @@
-<?php
-echo dirname($_SERVER['SCRIPT_NAME']);
+<?php if(isset($_GET['id']))
+{
+	echo $_GET['id'];
+}
 ?>
+<div class="container-fluid">
+	<div class="row text-center">
+		<h1>Bienvenue sur le M2L Gestion, suivi et administration de vos formations</h1>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-6 menu-formation">
+			<div class="row">
+				<div class="col-xs-12">	
+					<h2>Dernières formation parues :</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-3"></div>
+				<div class="col-xs-8 col-sm-6">
+					<h3>Nom de la formation</h3>
+					<h4>Date de la formation</h4>
+					<p class="hidden-xs">
+						<span class="label label-success">Genre</span>
+						<span class="label label-info">Formation</span>
+						<span class="label label-warning">Genre</span>
+						<span class="label label-danger">Formation</span>
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-3"></div>
+				<div class="col-xs-8 col-sm-6">
+					<h3>Nom de la formation</h3>
+					<h4>Date de la formation</h4>
+					<p class="hidden-xs">
+						<span class="label label-success">Genre</span>
+						<span class="label label-info">Formation</span>
+						<span class="label label-warning">Genre</span>
+						<span class="label label-danger">Formation</span>
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-3"></div>
+				<div class="col-xs-8 col-sm-6">
+					<h3>Nom de la formation</h3>
+					<h4>Date de la formation</h4>
+					<p class="hidden-xs">
+						<span class="label label-success">Genre</span>
+						<span class="label label-info">Formation</span>
+						<span class="label label-warning">Genre</span>
+						<span class="label label-danger">Formation</span>
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-3"></div>
+				<div class="col-xs-8 col-sm-6">
+					<h3>Nom de la formation</h3>
+					<h4>Date de la formation</h4>
+					<p class="hidden-xs">
+						<span class="label label-success">Genre</span>
+						<span class="label label-info">Formation</span>
+						<span class="label label-warning">Genre</span>
+						<span class="label label-danger">Formation</span>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6">
+			<h2>Calendrier des formations :</h2>
+		</div>
+	</div>
+</div>
